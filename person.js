aad51287627b9714cdf1a66662727857f2226c45
Anna Gosme <anna.gosme@gmail.com>
2021-06-22 12:42:50 +0200

    first commit

---
+++ person.js
@@ -0,0 +1,13 @@
+import Animal from './Animal.js'
+
+class Person extends Animal{
+    constructor(name, type) {
+        this.color = color;
+        this.model = model;
+        this.type = type;
+    }
+
+    greet() {
+
+    }
+}
